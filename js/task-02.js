c6f98f38bc1ea28f5e5c27849f06eb89aaa2e222
yuliia-andriitsova <andriitsovayulia@gmail.com>
2022-09-03 21:52:30 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,27 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const newIngredients = [];
+
+ingredients.forEach((ingredient) => {
+  const newItem = document.createElement('li');
+  newItem.textContent = ingredient;
+  newItem.classList.add('item');
+  newIngredients.push(newItem);
+});
+
+list.append(...newIngredients);
+console.log(list);
+
+// let newItem = document.createElement('li');
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   newItem[i] = ingredients[i];
+
+//   newItem.textContent = ingredients[i];
+
+//   console.log(newItem);
+// }
